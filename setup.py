8990d8a116e0a7b6db72770eb97e0fb168a4b82f
Dusan Klinec <dusan.klinec@gmail.com>
2017-10-30 16:41:40 -0500

    v1.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '1.0.8'
+version = '1.0.9'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
