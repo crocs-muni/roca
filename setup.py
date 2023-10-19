048988bae13b3025d389cc535fcb99063ac63bd0
Lumir Balhar <lbalhar@redhat.com>
2023-10-19 13:54:55 +0200

    Remove dependency on future and drop Python 2

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ install_requires = [
     'cryptography>=1.2.3',
     'setuptools>=1.0',
     'six',
-    'future',
     'coloredlogs',
     'pgpdump',
     'python-dateutil',
@@ -56,8 +55,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -67,7 +64,7 @@ setup(
 
     packages=find_packages(),
     include_package_data=True,
-    python_requires='>=2.7.10,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    python_requires='>=3.4',
     install_requires=install_requires,
     extras_require={
         'dev': dev_extras,
